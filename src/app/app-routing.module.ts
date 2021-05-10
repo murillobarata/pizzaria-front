0ec1f7aad932fada5211c4b499d48b8b13d81a0c
Murillo Barata <murillo.barata@lead.fortaleza>
2021-05-10 18:01:08 -0300

    Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateCustomerComponent } from './_pages/create-customer/create-customer.component';
+import { CustomersComponent } from './_pages/customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customers', component: CustomersComponent },
+  { path: 'create-customer', component: CreateCustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CustomersComponent } from './_pages/customers/customers.component';
+import { CreateCustomerComponent } from './_pages/create-customer/create-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule,
